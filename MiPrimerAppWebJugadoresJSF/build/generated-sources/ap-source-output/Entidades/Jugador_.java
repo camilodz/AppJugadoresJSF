f87fcbe3ab1c89fbe4ca2ca30316fdd3ff8d975a
Jorge Tunubala <jorgetunubala@unicauca.edu.co>
2018-02-27 08:03:34 -0500

    nueva lliadfadf

--- MiPrimerAppWebJugadoresJSF/build/generated-sources/ap-source-output/Entidades/Jugador_.java
+++ MiPrimerAppWebJugadoresJSF/build/generated-sources/ap-source-output/Entidades/Jugador_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-25T11:03:04")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-27T08:03:02")
 @StaticMetamodel(Jugador.class)
 public class Jugador_ { 
 
